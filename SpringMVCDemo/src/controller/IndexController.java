758a3a42ea3e2937ec48c9c810a7f7b193aec620
chu LIU <chu.liu.ext@nokia.com>
2017-07-26 23:01:53 +0800

    Add more function

--- SpringMVCDemo/src/controller/IndexController.java
+++ SpringMVCDemo/src/controller/IndexController.java
@@ -1,20 +1,51 @@
 package controller;
 
+import model.FullName;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+//import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 
 /**
  * Created by chuliu on 2017/7/25.
  */
-public class IndexController implements Controller{
+@Controller
+public class IndexController{
 
-    @Override
+    @RequestMapping("/index")
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
 
-        ModelAndView mav = new ModelAndView("index");
+        ModelAndView mav = new ModelAndView("redirect:/name");
+        return mav;
+    }
+
+    @RequestMapping("/name")
+    public ModelAndView Name(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+
+        ModelAndView mav = new ModelAndView("input_name");
+        return mav;
+    }
+
+    @RequestMapping("/submitName")
+    public ModelAndView submitName(FullName fullName) throws Exception {
+        //System.out.println("First name: "+fullName.getFirstName()+" | Last name:"+fullName.getLastName());
+        ModelAndView mav = new ModelAndView("show_full_name");
+        return mav;
+    }
+
+    @RequestMapping("/count")
+    public ModelAndView count(HttpSession session) throws Exception{
+        Integer i = (Integer) session.getAttribute("number");
+        if (i == null)
+            i = 0;
+        i++;
+        session.setAttribute("number", i);
+        ModelAndView mav = new ModelAndView("count");
         return mav;
     }
 }
--- SpringMVCDemo/web/WEB-INF/page/index.jsp
+++ SpringMVCDemo/web/WEB-INF/page/index.jsp
@@ -6,22 +6,7 @@
 
 <body>
 
-<form action="action_page.php" method="GET" target="_blank" accept-charset="UTF-8"
-      ectype="application/x-www-form-urlencoded" autocomplete="off" novalidate>
-
-    <fieldset>
-        <legend>Personal information:</legend>
-        First name:<br>
-        <input type="text" name="firstname" value="Mickey">
-        <br>
-        Last name:<br>
-        <input type="text" name="lastname" value="Mouse">
-        <br><br>
-        <input type="submit" value="Submit">
-    </fieldset>
-
-
-</form>
+<h2>MVC Index!</h2>
 
 </body>
 </html>
