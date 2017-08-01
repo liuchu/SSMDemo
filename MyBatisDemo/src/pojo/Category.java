241287eec36ed330bfbcf96f422408e286801615
Chu LIU <liuchuu@126.com>
2017-08-01 22:33:05 +0800

    Add some files in MyBatis module

---
+++ MyBatisDemo/src/pojo/Category.java
@@ -0,0 +1,42 @@
+package pojo;
+
+/**
+ * Created by chuliu on 2017/8/1.
+ */
+public class Category {
+
+    private int id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
